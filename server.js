1e6994f7ed549acb6d1f1d38f8c01bdd62710e62
CKPAY <nahomshimelis798@gmail.com>
2026-08-26 18:22:10 +0300

    Add withdraw routes to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const depositRoutes = require('./routes/deposit');
+const withdrawRoutes = require('./routes/withdraw');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/deposit', depositRoutes);
+app.use('/api/withdraw', withdrawRoutes);
 app.use('/api/v1/payment', depositRoutes);
 app.use('/api/v1/invoice', depositRoutes);
 app.use('/api/admin', adminRoutes);
